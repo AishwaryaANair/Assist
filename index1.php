be12cd11928f3893b989e11a0e68f4110df8b5bf
AishwaryaANair <2017.aishwarya.nair@ves.ac.in>
2019-10-03 10:32:47 +0530

    new

--- index1.php
+++ index1.php
@@ -11,17 +11,11 @@ if(isset($_POST['distance']))
     $SQL = "INSERT INTO distance (distance) VALUES ('$distance')";
     mysqli_query($conn,$SQL);
 
-    CloseCon($conn);
-
-}
-
-if(isset($_POST['lat']))
-{
     $lat=$_POST['lat'];
     $long=$_POST['long'];
     echo "$lat";
     echo "$long";
-    $SQL = "INSERT INTO location (lat,long) VALUES ('$lat','$long')";
+    $SQL = "INSERT INTO location (lat,longitude) VALUES ('$lat','$long')";
     mysqli_query($conn,$SQL);
 
     CloseCon($conn);
