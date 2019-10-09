0bc391d979ccacb70d3aff9614b7ce67269312f3
AishwaryaANair <2017.aishwarya.nair@ves.ac.in>
2019-10-09 20:18:33 +0530

    Final Html

--- saveImage.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    $info = pathinfo($_FILES['image']['name']);
-    $ext = $info['extension'];
-    $newname = 'newImage.'.$ext;
-    $target = 'images/'.$newname;
-    move_uploaded_file($_FILES['image']['tmp_name'],$target);
-
-?>
